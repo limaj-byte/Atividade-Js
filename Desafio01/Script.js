379e14a4a76f95476002d0b71589497c07d4ffb5
limaj-byte <lima.j@estudante.ifro.edu.br>
2026-06-01 17:40:42 -0400

    Update Script.js

--- Desafio01/Script.js
+++ Desafio01/Script.js
@@ -1,23 +1,39 @@
-let botao= document.querySelector("#botao");
-let input= document.querySelector("#input");
-let list= document.querySelector("#list");
-let total= document.querySelector("#total");
+let botao = document.querySelector("#botao");
+let input = document.querySelector("#input");
+let list = document.querySelector("#list");
+let total = document.querySelector("#total");
 
-let contador=0;
+let contador = 0;
 
-botao.addEventListener("click", function()
-{
-     if (input.value ===""){
+botao.addEventListener("click", function() {
+    if (input.value.trim() === "") {
         alert("Insira um item para adicionar a lista!");
         return;
     }
-    list.innerHTML += `
-    <div class="itens">
-        <li>${input.value}</li>
-        <button class="concluir">Concluir</button>
-        <button class="delete">Excluir</button>
-    </div>
+
+    let lista = document.createElement("li");
+    lista.innerHTML = `
+        <span>${input.value}</span>
+        <div>
+            <button class="concluir">Concluir</button>
+            <button class="delete">Excluir</button>
+        </div>
     `;
+
+    list.appendChild(lista);
     contador++;
     total.innerHTML = `Total: ${contador}`;
+    input.value = "";
+
+    let concluir = lista.querySelector(".concluir");
+    concluir.addEventListener("click", function() {
+        lista.style.textDecoration = "line-through";
+    });
+
+    let excluir = lista.querySelector(".delete");
+    excluir.addEventListener("click", function() {
+        list.removeChild(lista);
+        contador--;
+        total.innerHTML = `Total: ${contador}`;
+    });
 });
